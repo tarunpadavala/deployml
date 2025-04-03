7af00a55678b41b1fa8957154b21548a1520e360
tarunpadavala <108194500+tarunpadavala@users.noreply.github.com>
2025-04-03 12:09:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ import pickle
 import numpy as np
 import joblib
 import xgboost
-# Load the trained model
+import randomforest
 
 
-# Load the model
-model = joblib.load("model.pkl")
+# Load the trained models
+facebook_model = joblib.load("facebook_model.pkl")
+twitter_model = joblib.load("twitter_model.pkl")
+instagram_model = joblib.load("instagram_model.pkl")
 
 app = Flask(__name__)
 
@@ -17,17 +19,35 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Extract data from form
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    
-    # Make prediction
-    prediction = model.predict(final_features)
-    output = 'Not Fake' if prediction[0] == 1 else 'Fake'
+@app.route('/facebook', methods=['GET', 'POST'])
+def facebook():
+    if request.method == 'POST':
+        int_features = [int(x) for x in request.form.values()]
+        final_features = [np.array(int_features)]
+        prediction = facebook_model.predict(final_features)
+        output = 'Not Fake' if prediction[0] == 1 else 'Fake'
+        return render_template('facebook.html', prediction_text='Facebook Prediction: {}'.format(output))
+    return render_template('facebook.html')
 
-    return render_template('index.html', prediction_text='Prediction: {}'.format(output))
+@app.route('/twitter', methods=['GET', 'POST'])
+def twitter():
+    if request.method == 'POST':
+        int_features = [int(x) for x in request.form.values()]
+        final_features = [np.array(int_features)]
+        prediction = twitter_model.predict(final_features)
+        output = 'Not Fake' if prediction[0] == 1 else 'Fake'
+        return render_template('twitter.html', prediction_text='Twitter Prediction: {}'.format(output))
+    return render_template('twitter.html')
+
+@app.route('/instagram', methods=['GET', 'POST'])
+def instagram():
+    if request.method == 'POST':
+        int_features = [int(x) for x in request.form.values()]
+        final_features = [np.array(int_features)]
+        prediction = instagram_model.predict(final_features)
+        output = 'Not Fake' if prediction[0] == 1 else 'Fake'
+        return render_template('instagram.html', prediction_text='Instagram Prediction: {}'.format(output))
+    return render_template('instagram.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
