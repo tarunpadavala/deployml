158d1d4ba015a47fb0381cef5cf3eed3013f8c62
tarunpadavala <108194500+tarunpadavala@users.noreply.github.com>
2025-04-03 15:27:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,30 @@
 # app.py
- 
- from flask import Flask, request, jsonify, render_template
- import pickle
- import numpy as np
- import joblib
- import xgboost
- # Load the trained model
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+import joblib
+import xgboost
+# Load the trained model
  
  
  # Load the model
- model = joblib.load("model.pkl")
+model = joblib.load("model.pkl")
  
- app = Flask(__name__)
+app = Flask(__name__)
  
- @app.route('/')
- def home():
-     return render_template('index.html')
+@app.route('/')
+def home():
+ return render_template('index.html')
  
- @app.route('/predict', methods=['POST'])
- def predict():
-     # Extract data from form
-     int_features = [int(x) for x in request.form.values()]
-     final_features = [np.array(int_features)]
+@app.route('/predict', methods=['POST'])
+def predict():
+ int_features = [int(x) for x in request.form.values()]
+ final_features = [np.array(int_features)]
+ prediction = model.predict(final_features)
+ output = 'Placed' if prediction[0] == 1 else 'Not Placed'
+ output = 'Not Fake' if prediction[0] == 1 else 'Fake'
+ return render_template('facebook.html', prediction_text='Prediction: {}'.format(output))
  
-     # Make prediction
-     prediction = model.predict(final_features)
-     output = 'Placed' if prediction[0] == 1 else 'Not Placed'
-     output = 'Not Fake' if prediction[0] == 1 else 'Fake'
- 
-     return render_template('facebook.html', prediction_text='Prediction: {}'.format(output))
- 
- if __name__ == "__main__":
-     app.run(debug=True)
+if __name__ == "__main__":
+ app.run(debug=True)
        
